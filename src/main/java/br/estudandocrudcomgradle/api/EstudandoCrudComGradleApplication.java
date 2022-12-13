97f76fee4d3d3c62e98820f90559cf00bed0b116
JeanHeberth <jeanheberth19@gmail.com>
2022-12-13 08:44:58 -0300

    init

---
+++ src/main/java/br/estudandocrudcomgradle/api/EstudandoCrudComGradleApplication.java
@@ -0,0 +1,13 @@
+package br.estudandocrudcomgradle.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoCrudComGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstudandoCrudComGradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/estudandocrudcomgradle/api/EstudandoCrudComGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.estudandocrudcomgradle.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandoCrudComGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
